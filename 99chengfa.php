820eceb6faefeb79beb14381f7720559e86b5f49
10989 <1098958975@qq.com>
2017-12-08 19:28:51 +0800

    1212

---
+++ 99chengfa.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 10989
+ * Date: 2017/12/8
+ * Time: 18:55
+ */
+
+for($i=1;$i<=9;$i++){
+    for ($j=1;$j<=$i;$j++){
+        echo $i."*".$j."=".$i*$j."  "    ;
+    }echo "</br>";
+}echo "</br>";
---
+++ test.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 10989
+ * Date: 2017/12/8
+ * Time: 15:33
+ */
+echo "<table>";
+echo "<caption><caption>";
+for ($i=0;$i<=100;$i++){
+    echo"<tr>";
+    for($j=0;$j<=10;$j++){
+        echo "<td>$i";
+
+        echo"</td>";
+    }
+    echo"</tr>";
+}
+echo "</table>";
